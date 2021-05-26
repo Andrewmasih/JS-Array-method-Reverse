93e50d97c0e987ef732cc2a9e45e572dccfe2511
Andrew Masih <armasih@outlook.com>
2021-05-26 15:10:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Reverse method is an action for an Array, .reverse(), reverses the items in the Array */
+
+let myArray = ["john","matthew","luke", "mark"];
+console.log ("im the array", myArray.reverse());
+
+let reversedArray = myArray.reverse();
+console.log ("im the reversed Array", reversedArray);
